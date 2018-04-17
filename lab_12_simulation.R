1a73e53b4e4c633e1e645f4a77f4f83c38c51ecf
Joie Feng <joiejoie@Joies-Air.wv.cc.cmu.edu>
2018-04-17 12:43:48 -0400

    generates data from std normal

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -2,7 +2,7 @@ generate_data = function(n, p) {
   cov = rnorm(n*p)
   cov.mat = matrix(cov, nrow = n, ncol = p)
   vec = rnorm(n)
-  result = list(cov, vec)
+  result = list(cov.mat, vec)
   names(result) = c("covariates", "responses")
   return(result)
 }
